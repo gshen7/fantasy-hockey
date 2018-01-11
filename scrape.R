568e54bfd7b167336e79213d5622f740a0a94501
gshen7 <gshen7@uwo.ca>
2018-01-10 22:19:47 -0500

    added script for updating data every day

--- scrape.R
+++ scrape.R
@@ -4,12 +4,15 @@
 library("rvest")
 library("XML")
 
+# initializer
 sPoints<-list()
 gPoints<-list()
+sPointsCum<-list()
+gPointsCum<-list()
 
+# TODO automatically get current day - starting day
 currentPeriod<-97
 
-# automatically get current day - starting day
 for(teamId in 1:11){
   s<-NA
   g<-NA
@@ -19,7 +22,6 @@ for(teamId in 1:11){
     day <- read_html(url)
     
     skaters <- html_nodes(day, xpath = '//*[@id="playertable_0"]')
-    head(skaters)
     skaters<-as(skaters, "character")
     skaters<-readHTMLTable(skaters)
     skaters<-as.data.frame(skaters)
@@ -29,7 +31,6 @@ for(teamId in 1:11){
     skaters<-Filter(function(x) !(all(x=="")), skaters)
     
     goalies <- html_nodes(day, xpath = '//*[@id="playertable_1"]')
-    head(goalies)
     goalies<-as(goalies, "character")
     goalies<-readHTMLTable(goalies)
     goalies<-as.data.frame(goalies)
@@ -57,15 +58,64 @@ for(teamId in 1:11){
 }
 
 for(team in 1:11){
-  sPoints[[team]]<-cumsum(sPoints[[team]])
-  gPoints[[team]]<-cumsum(gPoints[[team]])
+  sPointsCum[[team]]<-cumsum(sPoints[[team]])
+  gPointsCum[[team]]<-cumsum(gPoints[[team]])
 }
 
+# updater
+newPeriod<-98
+for(teamId in 1:11){
+  s<-NA
+  g<-NA
+  for(scoringPeriod in (currentPeriod+1):newPeriod){
+    url <- paste("http://games.espn.com/fhl/clubhouse?leagueId=34619&teamId=",teamId,"&seasonId=2018&scoringPeriodId=", scoringPeriod, sep="")
+    
+    day <- read_html(url)
+    
+    skaters <- html_nodes(day, xpath = '//*[@id="playertable_0"]')
+    skaters<-as(skaters, "character")
+    skaters<-readHTMLTable(skaters)
+    skaters<-as.data.frame(skaters)
+    skaters <- data.frame(lapply(skaters, as.character), stringsAsFactors=FALSE)
+    colnames(skaters)<-skaters[1,]
+    skaters<-skaters[-1,]
+    skaters<-Filter(function(x) !(all(x=="")), skaters)
+    
+    goalies <- html_nodes(day, xpath = '//*[@id="playertable_1"]')
+    goalies<-as(goalies, "character")
+    goalies<-readHTMLTable(goalies)
+    goalies<-as.data.frame(goalies)
+    goalies <- data.frame(lapply(goalies, as.character), stringsAsFactors=FALSE)
+    colnames(goalies)<-goalies[1,]
+    goalies<-goalies[-1,]
+    goalies<-Filter(function(x) !(all(x=="")), goalies)
+    
+    skatersTotals<-skaters[skaters$SLOT == "",]
+    colnames(skatersTotals)<-colnames(skatersTotals)[-(1:2)]
+    skatersTotals<-skatersTotals[,-(c(1:2,(ncol(skatersTotals)-3):ncol(skatersTotals)))]
+    colnames(skatersTotals)[1:2]<-""
+    # skatersTotals[1,]$PTS
+    sPoints[[teamId]]<-c(sPoints[[teamId]],skatersTotals[1,]$PTS)
+    
+    goaliesTotals<-goalies[goalies$SLOT == "",]
+    colnames(goaliesTotals)<-colnames(goaliesTotals)[-(1:2)]
+    goaliesTotals<-goaliesTotals[,-(c(1:2,(ncol(goaliesTotals)-3):ncol(goaliesTotals)))]
+    colnames(goaliesTotals)[1:2]<-""
+    # goaliesTotals[1,]$PTS
+    gPoints[[teamId]]<-c(gPoints[[teamId]],goaliesTotals[1,]$PTS)
+  }
+}
+
+for(team in 1:11){
+  sPointsCum[[team]]<-cumsum(sPoints[[team]])
+  gPointsCum[[team]]<-cumsum(gPoints[[team]])
+}
+
+# plotter
 COLOURS<-c("#f44242", "#f4a941", "#f2ec3e", "#259b31", "#4fc1ea", "#1627a3", "#aa76f7", "#ed5ac8", "black", "grey", "brown")
 TEAMS<-c("Devin", "Matt", "Evan", "Dave", "Brett", "Jory", "Gary", "Carl", "Kyle", "Ryan", "Tanner")
-plot(1:currentPeriod, sPoints[[1]]+gPoints[[1]], type="l", col=COLOURS[1],xlim=c(0,100),ylim=c(0,3000))
+plot(1:length(sPointsCum[[1]]), sPointsCum[[1]]+gPointsCum[[1]], type="l", col=COLOURS[1],xlim=c(0,100),ylim=c(0,3000))
 for(team in 2:11){
-  lines(1:currentPeriod, sPoints[[team]]+gPoints[[team]], type="l", col=COLOURS[team])
+  lines(1:length(sPointsCum[[team]]), sPointsCum[[team]]+gPointsCum[[team]], type="l", col=COLOURS[team])
 }
 legend(0, 3000, legend=TEAMS, col=COLOURS, lty=1, cex=0.8)
-
